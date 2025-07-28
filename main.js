de8c04d03063e723dc7052cda5ebceaf4b33b5c8
MuthuKr1shhnan <2000muthukrishnan@gmail.com>
2025-07-28 13:24:04 +0530

    creating routes
    Signed-off-by: MuthuKr1shhnan <2000muthukrishnan@gmail.com>

--- main.js
+++ main.js
@@ -6,6 +6,19 @@ app.get('/', (req, res) => {
   res.json({msg : 'Hello World!'})
 })
 
+//CRUD Operation
+//C - For creating
+app.post('/movies', (req, res)=>{})
+
+//R - For Reading
+app.post('/movies', (req, res)=>{})
+
+//U - For Updating
+app.post('/movies:id', (req, res)=>{})
+
+//D - For Deleting
+app.post('/movies:id', (req, res)=>{})
+
 const port = 6969
 
 app.listen(port, () => {
